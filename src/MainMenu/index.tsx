f930144d980f31ba4e28b949fc756e19236d3bd0
Michael Bruce Allen <CodeAmend@Gmail.Com>
2020-04-30 18:12:32 -0400

    Refactor project LightsOut and MainMenu folder

--- src/MainMenu/index.tsx
+++ src/MainMenu/index.tsx
@@ -1,17 +1,14 @@
 import React from 'react';
 
 import { MainScreen, Header } from './styles';
+import LightsOut from '../LightsOut';
 
 
 function App() {
   return (
     <MainScreen>
       <Header>React Games</Header>
-      <ul>
-        <li>Lights Out</li>
-        <li>Snake</li>
-        <li>Game Of Life</li>
-      </ul>
+      <LightsOut />
     </MainScreen>
   );
 }
